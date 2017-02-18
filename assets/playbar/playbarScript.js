6b7067f05ad9690c08148ba0f9fae2cb8ca3b9d6
Juan Manuel Sánchez <juanmanuel.sanchez@icloud.com>
2017-02-18 11:50:38 +0100

    Initial commit

---
+++ assets/playbar/playbarScript.js
@@ -0,0 +1,52 @@
+cp.playbarAssetArr = 
+[
+	'AudioOff',
+	'AudioOn',
+	'BackGround',
+	'Backward',
+	'Color',
+	'ColorSmall',
+	'CC',
+	'Exit',
+	'FastForward',
+	'FastForward1',
+	'FastForward2',
+	'Forward',
+	'Glow',
+	'GlowSmall',
+	'Height',
+	'InnerStroke',
+	'InnerStrokeSmall',
+	'Play',
+	'Pause',
+	'Progress',
+	'Rewind',
+	'Shade',
+	'ShadeSmall',
+	'Stroke',
+	'StrokeSmall',
+	'Thumb',
+	'ThumbBase',
+	'TOC'
+];
+cp.playbarTooltips = 
+{
+	AudioOff : "Audio act.",
+	AudioOn : "Audio desact. ",
+	Backward : "Atrás ",
+	CC : "Subtítulos opcionales ",
+	Exit : "Salir ",
+	FastForward : "Velocidad de avance rápido 2x ",
+	FastForward1 : "Velocidad de avance rápido 4x ",
+	FastForward2 : "Velocidad normal ", 
+	Forward : "Adelante ",
+	Play : "Reproducir ",
+	Pause : "Pausa ",
+	Rewind : "Rebobinar ",
+	TOC : "Contenido ",
+	Info : "Información ",
+	Print : "Imprimir "
+};
+cp.handleSpecialForPlaybar = function(playbarConstruct)
+{
+}
